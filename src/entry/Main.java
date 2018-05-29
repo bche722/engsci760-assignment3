1d3d339d3cf7c51656019007eabb1b6011c6531f
bche722 <bche722@aucklanduni.ac.nz>
2018-05-29 17:18:18 +1200

    tabuSearch done

--- src/entry/Main.java
+++ src/entry/Main.java
@@ -7,23 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javafx.util.Pair;
+
 public class Main {
 
-	private static final String fileName= "ProbC" ;
-	
+	private static final String fileName = "ProbA";
+
 	private static double[] weight;
 	private static double[][] position;
 	private static int[] solution;
-	
+
 	private static List<Double> bestMeasures;
 	private static List<Double> tempMeasures;
 
 	public static void main(String args[]) {
-		weightReader("src/data/"+fileName+".txt");
+		weightReader("src/data/" + fileName + ".txt");
 		positionReader("src/data/Positions.txt");
 		bestMeasures = new ArrayList<Double>();
 		tempMeasures = new ArrayList<Double>();
-		nextDescentSearch200(solution);
+		// nextDescentSearch200(solution);
+		tabuSearch(solution);
 	}
 
 	public static void weightReader(String path) {
@@ -36,7 +39,7 @@ public class Main {
 			weight[0] = 0;
 			for (int i = 1; i < lineNumber + 1; i++) {
 				weight[i] = Double.parseDouble(scanner.nextLine().trim());
-				solution[i-1]=i;
+				solution[i - 1] = i;
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
@@ -81,6 +84,68 @@ public class Main {
 		return result;
 	}
 
+	public static void tabuSearch(int[] s) {
+		boolean[][] flag = new boolean[120][120];
+		int banListSize = Math.min(20, weight.length / 3);
+		int[][] banList = new int[banListSize][2];
+		int count = 0;
+		int worsen = 50000;
+
+		double currentX = 0;
+		double currentY = 0;
+		double sumWeight = 0;
+		for (int i = 0; i < 120; i++) {
+			currentX += weight[s[i]] * position[i][0];
+			currentY += weight[s[i]] * position[i][1];
+			sumWeight += weight[s[i]];
+		}
+		int[] current = s.clone();
+		while (count < 100000 && count < worsen * 2) {
+			System.out.println((Math.abs(currentX) + 5 * Math.abs(currentY)) / sumWeight);
+			int[] currentPosition = new int[2];
+			double iterationBestX = Double.POSITIVE_INFINITY;
+			double iterationBestY = Double.POSITIVE_INFINITY;
+			for (int a = 0; a < 120; a++) {
+				for (int b = a + 1; b < 120; b++) {
+					if (flag[a][b] || (weight[current[a]] == 0 && weight[current[b]] == 0) || a == b - 60) {
+						continue;
+					} else {
+						double tempX = currentX + weight[current[a]] * position[b][0]
+								+ weight[current[b]] * position[a][0] - weight[current[a]] * position[a][0]
+								- weight[current[b]] * position[b][0];
+						double tempY = currentY + weight[current[a]] * position[b][1]
+								+ weight[current[b]] * position[a][1] - weight[current[a]] * position[a][1]
+								- weight[current[b]] * position[b][1];
+						double tempMeasure = Math.abs(tempX) + 5 * Math.abs(tempY);
+						double iterationBestMeasure = Math.abs(iterationBestX) + 5 * Math.abs(iterationBestY);
+						if (tempMeasure < iterationBestMeasure) {
+							iterationBestX = tempX;
+							iterationBestY = tempY;
+							currentPosition[0] = a;
+							currentPosition[1] = b;
+						}
+					}
+				}
+			}
+			int temp = current[currentPosition[0]];
+			current[currentPosition[0]] = current[currentPosition[1]];
+			current[currentPosition[1]] = temp;
+			flag[banList[count % banListSize][0]][banList[count % banListSize][1]] = false;
+			flag[currentPosition[0]][currentPosition[1]] = true;
+			banList[count % banListSize][0] = currentPosition[0];
+			banList[count % banListSize][1] = currentPosition[1];
+			double currentMeasure = Math.abs(currentX) + 5 * Math.abs(currentY);
+			double iterationBestMeasure = Math.abs(iterationBestX) + 5 * Math.abs(iterationBestY);
+			if (currentMeasure < iterationBestMeasure && worsen == 50000) {
+					worsen = count;
+			}
+			currentX = iterationBestX;
+			currentY = iterationBestY;
+			count++;
+		}
+		System.out.println(count);
+	}
+
 	public static void nextDescentSearch2(int[] s) {
 		int[] bestSolution = s.clone();
 		double bestMeasure = getMeasure(bestSolution);
@@ -110,17 +175,17 @@ public class Main {
 		int[] bestSolution = s.clone();
 		for (int i = 0; i < 200; i++) {
 			shuffle(s);
-			int[] newSolution =nextDescentSearch(s);
-			if(getMeasure(newSolution) < getMeasure(bestSolution)) {
-				bestSolution=newSolution;
+			int[] newSolution = nextDescentSearch(s);
+			if (getMeasure(newSolution) < getMeasure(bestSolution)) {
+				bestSolution = newSolution;
 			}
 		}
-		
+
 		try {
-			PrintWriter pw = new PrintWriter("src/"+fileName+"output.txt");
-			pw.write(getMeasure(bestSolution)+ "\n");
+			PrintWriter pw = new PrintWriter("src/" + fileName + "output.txt");
+			pw.write(getMeasure(bestSolution) + "\n");
 			for (int i = 0; i < bestSolution.length; i++) {
-				pw.write(bestSolution[i]+ "\n");
+				pw.write(bestSolution[i] + "\n");
 			}
 			pw.flush();
 			pw.close();
